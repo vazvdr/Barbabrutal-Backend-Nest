13e1cab07b984a73fab06ca0f213d2f41c099850
vazvdr <azevedo.vanderson@hotmail.com>
2024-12-27 15:43:29 -0300

    Habilitando o cors

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
-  const port = process.env.PORT || 3001;
-  await app.listen(port);
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: 'https://barbabrutal.vercel.app',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
+  await app.listen(process.env.PORT || 3001);
 }
 bootstrap();
