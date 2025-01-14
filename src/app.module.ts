883beca8e3eb14f2a705bf8e0a4b10ee2d699d6c
vazvdr <azevedo.vanderson@hotmail.com>
2025-01-14 16:19:10 -0300

    the change didn't to work on app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,10 @@ import { ServicoModule } from './servico/servico.module';
 import { AgendamentoModule } from './agendamento/agendamento.module';
 import { UsuarioModule } from './usuario/usuario.module';
 import { ProfissionalModule } from './profissional/profissional.module';
-import { UsuarioController } from './usuario/usuario.controller';
-import { UsuarioMiddleware } from './usuario/usuario.middleware';
 
 @Module({
   imports: [DbModule, ServicoModule, AgendamentoModule, UsuarioModule, ProfissionalModule],
-  controllers: [UsuarioController],
+  controllers: [],
   providers: [],
 })
 export class AppModule {}
