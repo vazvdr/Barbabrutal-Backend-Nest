e7489df0b6f1e0108f3dac92e8689cea8363c43e
vazvdr <azevedo.vanderson@hotmail.com>
2025-01-14 16:13:13 -0300

    Sending changes on app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,21 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { DbModule } from './db/db.module';
 import { ServicoModule } from './servico/servico.module';
 import { AgendamentoModule } from './agendamento/agendamento.module';
 import { UsuarioModule } from './usuario/usuario.module';
 import { ProfissionalModule } from './profissional/profissional.module';
+import { UsuarioController } from './usuario/usuario.controller';
+import { UsuarioMiddleware } from './usuario/usuario.middleware';
 
 @Module({
   imports: [DbModule, ServicoModule, AgendamentoModule, UsuarioModule, ProfissionalModule],
-  controllers: [],
+  controllers: [UsuarioController],
   providers: [],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(UsuarioMiddleware)
+      .forRoutes('usuario/alterar', 'usuario/excluir'); 
+  }
+}
