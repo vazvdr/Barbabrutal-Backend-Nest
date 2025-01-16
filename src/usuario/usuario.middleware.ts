7b9e62eb8c28f2ebdfb9c3aff7d99afdf608185b
vazvdr <azevedo.vanderson@hotmail.com>
2025-01-15 21:16:14 -0300

    Sending changes

--- src/usuario/usuario.middleware.ts
+++ src/usuario/usuario.middleware.ts
@@ -1,28 +1,18 @@
-import { HttpException, Injectable, NestMiddleware } from '@nestjs/common';
+import { Injectable, NestMiddleware, HttpException } from '@nestjs/common';
 import { Request, Response, NextFunction } from 'express';
-import { UsuarioRepository } from './usuario.repository';
-import * as jwt from 'jsonwebtoken';
-import { Usuario } from '../regras';
 
 @Injectable()
 export class UsuarioMiddleware implements NestMiddleware {
-  constructor(private readonly repo: UsuarioRepository) {}
+    use(req: Request, res: Response, next: NextFunction) {
+        // Lista de métodos HTTP permitidos
+        const allowedMethods = ['GET', 'POST', 'PUT', 'DELETE'];
 
-  async use(req: Request, res: Response, next: NextFunction) {
-    const token = req.headers['authorization']?.replace('Bearer ', '');
+        // Verifica se o método HTTP da requisição é permitido
+        if (!allowedMethods.includes(req.method)) {
+            throw new HttpException(`Método ${req.method} não permitido`, 405);
+        }
 
-    if (!token) {
-      throw new HttpException('Token não informado', 401);
+        // Continua o fluxo se o método for permitido
+        next();
     }
-
-    const payload = jwt.verify(token, process.env.JWT_SECRET!) as Usuario;
-    const usuario = await this.repo.buscarPorEmail(payload.email!);
-
-    if (!usuario) {
-      throw new HttpException('Usuário não encontrado', 401);
-    }
-
-    (req as any).usuario = usuario;
-    next();
-  }
 }
