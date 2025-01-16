5bd7d8b70d209cfa6c7fc6d05460dc5c22f31781
vazvdr <azevedo.vanderson@hotmail.com>
2025-01-16 12:14:10 -0300

    Sending changes on usuario decorator

--- src/usuario/usuario.decorator.ts
+++ src/usuario/usuario.decorator.ts
@@ -1,8 +1,8 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 
 export const UsuarioLogado = createParamDecorator(
-  (_data: unknown, ctx: ExecutionContext) => {
+  (data: unknown, ctx: ExecutionContext) => {
     const request = ctx.switchToHttp().getRequest();
-    return request.usuario;
+    return request.usuarioLogado;
   },
 );
