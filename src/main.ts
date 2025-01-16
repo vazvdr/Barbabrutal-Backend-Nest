cb2c429fccadb91b3ca505c804f47bea478edf62
vazvdr <azevedo.vanderson@hotmail.com>
2025-01-15 21:20:26 -0300

    Sending changes on main ts

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-
-  app.enableCors({
-    origin: 'https://barbabrutal.vercel.app',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-  });
-
-  await app.listen(process.env.PORT || 3001);
+    const app = await NestFactory.create(AppModule);
+    app.enableCors({
+        origin: 'https://barbabrutal.vercel.app', // Permitir somente o frontend
+        methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos permitidos
+    });
+    await app.listen(3000);
 }
 bootstrap();
