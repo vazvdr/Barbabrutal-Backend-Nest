56b15ada17240029cae48c517fcb90b56b3d83dc
vazvdr <azevedo.vanderson@hotmail.com>
2025-01-22 00:17:49 -0300

    Allowing localhost requisition

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,12 @@ import { AppModule } from './app.module';
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.enableCors({
-        origin: 'https://barbabrutal.vercel.app', // Permitir somente o frontend
-        methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos permitidos
+        origin: [
+            'https://barbabrutal.vercel.app',
+            'http://localhost:3000',
+        ],
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
     });
-    await app.listen(3000);
+    await app.listen(3001);
 }
 bootstrap();
